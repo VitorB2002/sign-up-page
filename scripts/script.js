85982a2c4413af4b6f4fe706dde08f2c109ffe95
VitorB2002 <vitorb.santos2002@gmail.com>
2022-12-28 10:23:33 -0300

    Password validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function checkForm(form){
+    
+    if(form.password.value == form.confirmPassword.value){
+        alert("Congrats, account created!");
+        return true;
+    }   else{
+        alert("Passwords dont match, try again!");
+        return false;
+    }
+}
